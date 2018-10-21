746d1f5b84da2daa2527d95c5af9d6b901b84796
ngmysamuel <e0148675@u.nus.edu>
2018-10-21 10:25:56 +0800

    Test Commit

--- MerlionHotelClient/src/java/merlionhotelclient/Main.java
+++ MerlionHotelClient/src/java/merlionhotelclient/Main.java
@@ -11,9 +11,6 @@ package merlionhotelclient;
  */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
     }
